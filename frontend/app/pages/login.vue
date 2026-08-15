feb53cf18f10b4cae6a9f1331b6bbb9acae04f8f
akai-keisanki <iago.ad@outlook.com>
2026-08-14 21:16:17 -0300

    feat(frontend): enhance login form

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -11,29 +11,44 @@
 
 <template>
   <page title=Login>
-    <div class=infobox>
+    <NuxtLink to=/logon><div class='infobox link'>
       <h4>
         Você é nov@ no En10da?
       </h4>
       <p>
-        Caso não possua uma conta, crie uma na página de <NuxtLink to=/logon class=link>logon</NuxtLink>.
+        Caso não possua uma conta, clique aqui para encaminhar-se à página de logon.
       </p>
-    </div>
+    </div></NuxtLink>
     <form @submit.prevent=makeLogin>
       <div class=max-500>
         <div class=input>
           <label for=handle>Identificador</label>
           <input id=handle type=text placeholder=identificador required v-model=handle>
         </div>
-        <div class=input>
-          <label for=password>Senha</label>
-          <input id=password type=password placeholder=senha required v-if=!emailLogin v-model=password>
+        <div class=horinput>
+          <label for=email_login>Login pelo e-mail</label>
+          <input id=email_login type=checkbox v-model=emailLogin>
         </div>
-        <div class=input>
-          <label for=email_code>Código do e-mail</label>
-          <input id=email_code v-if=emailLogin v-model=email_code>
-        </div>
-        <button type=submit class=highlight></button>
+        <Transition name=fade>
+          <div class='input disappears' v-show=!emailLogin>
+            <label for=password>Senha</label>
+            <input id=password type=password placeholder=******** required  v-model=password>
+          </div>
+        </Transition>
+        <Transition name=fade>
+          <NuxtLink to=/send-email-code><div class='infobox link disappears' v-show=emailLogin>
+            <p>
+              Caso não possua um código de e-mail, clique aqui para encaminhar-se ao formulário de envio deste código.
+            </p>
+          </div></NuxtLink>
+        </Transition>
+        <Transition name=fade>
+          <div class='input disappears' v-show=emailLogin>
+            <label for=email_code>Código do e-mail</label>
+            <input id=email_code placeholder=... v-model=emailCode>
+          </div>
+        </Transition>
+        <button type=submit class=highlight>Login</button>
       </div>
     </form>
   </page>
