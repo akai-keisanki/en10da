965d480346fa8e5ed9baf025b68aaf5af7b5a12d
akai-keisanki <iago.ad@outlook.com>
2026-08-16 20:26:52 -0300

    feat(fronend): add channel index page

---
+++ frontend/app/components/post-sum.vue
@@ -0,0 +1,24 @@
+<script setup>
+  const props = defineProps({
+    data: {
+      type: Object,
+      required: true
+    },
+  })
+</script>
+
+<template>
+  <NuxtLink :to='`/${data.author.handle}/${data.channel.handle}/${data.handle}`'><div class=post-sum>
+    <div class='cttcol_l'>
+      <h4>{{data.title}}</h4>
+      <div class=path>{{data.author.handle}}/{{data.channel.handle}}/{{data.handle}}</div>
+    </div>
+    <div class=cttcol_r>
+      <div class=lndl>
+        <div class=likes>+{{data.like_count}}</div>
+        <div class=dislikes>-{{data.dislike_count}}</div>
+      </div>
+      <div class=datetime>{{data.creation_datetime}}</div>
+    </div>
+  </div></NuxtLink>
+</template>
--- frontend/app/pages/[user]/[channel]/index.vue
+++ frontend/app/pages/[user]/[channel]/index.vue
@@ -3,7 +3,32 @@
   const route = useRoute()
   const user = route.params.user
   const channel = route.params.channel
+
+  const {get} = useAPI()
+
+  const chResp = await get(`channel/${user}/${channel}`)
+  const {name, author: {name: authorName}} = chResp.data
+
+  const pstResp = await get(`post/search`, {
+    params: {
+      author_handle: user,
+      channel_handle: channel,
+      order_by_oldest: true
+    }
+  })
+  const posts = pstResp.data.posts
 </script>
 
 <template>
+  <page :title=name :subtitle='`${user}/${channel}`'>
+    <div class=post-list>
+      <div class=max-500>
+        <post-sum
+          v-for='post in posts'
+          :key=post.id
+          :data=post
+        ></post-sum>
+      </div>
+    </div>
+  </page>
 </template>
